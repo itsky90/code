70c98c2281795257890979bd2d38723e6524a412
itsky90 <itsky90@gmail.com>
2017-07-19 15:03:28 +0700

    add css js

--- webshopstar/get.py
+++ webshopstar/get.py
@@ -1,6 +1,6 @@
 import requests as re
 from bs4 import BeautifulSoup
-import urllib
+import urllib,os
 
 r = re.get('http://shopstars.myzozo.net')
 soup = BeautifulSoup(r.text, 'html.parser')
@@ -8,12 +8,30 @@ soup = BeautifulSoup(r.text, 'html.parser')
 for i in soup.head.find_all('link'):
     l =  'http://shopstars.myzozo.net/'+i.get('href')
     name = i.get('href').split('/')[-1]
-    print name
-    print "downloading " + name
+    _dir = os.path.dirname(i.get('href'))
+    root_dir = os.getcwd()
+    try:
+        os.makedirs(_dir)
+    except:
+        pass
+    os.chdir(_dir)
+    print "downloading " + i.get('href')
     urllib.urlretrieve(l, name)
+    os.chdir(root_dir)
 
 for i in soup.head.find_all('script'):
-    print 'http://shopstars.myzozo.net/'+i.get('src')
+    l = 'http://shopstars.myzozo.net/'+i.get('src')
+    name = i.get('src').split('/')[-1]
+    _dir = os.path.dirname(i.get('src'))
+    root_dir = os.getcwd()
+    try:
+        os.makedirs(_dir)
+    except:
+        pass
+    os.chdir(_dir)
+    print "downloading " + i.get('src')
+    urllib.urlretrieve(l, name)
+    os.chdir(root_dir)
 
 
 #print soup.head
